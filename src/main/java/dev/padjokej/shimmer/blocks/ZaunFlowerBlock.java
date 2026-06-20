cfc089724e72757b0c5cd5ae630b32164102c276
PadjokeJ <jonatan@padjokej.dev>
2026-06-20 15:21:05 +0200

    feat: renewable shimmer

--- src/main/java/dev/padjokej/shimmer/blocks/ZaunFlowerBlock.java
+++ src/main/java/dev/padjokej/shimmer/blocks/ZaunFlowerBlock.java
@@ -3,17 +3,21 @@ package dev.padjokej.shimmer.blocks;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.ScheduledTickAccess;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
-public class ZaunFlowerBlock extends Block {
+public class ZaunFlowerBlock extends Block implements BonemealableBlock {
     public static final MapCodec<ZaunFlowerBlock> CODEC = simpleCodec(ZaunFlowerBlock::new);
 
     public static final IntegerProperty FLOWER_AMOUNT = IntegerProperty.create("flower_amount", 1, 4);
@@ -54,5 +58,20 @@ public class ZaunFlowerBlock extends Block {
     protected boolean canSurvive(final BlockState state, final LevelReader level, final BlockPos pos) {
         BlockPos below = pos.below();
         return !level.getBlockState(below).is(BlockTags.AIR);
+    }
+
+    @Override
+    public boolean isValidBonemealTarget(final LevelReader level, final BlockPos pos, final BlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean isBonemealSuccess(final Level level, final RandomSource random, final BlockPos pos, final BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void performBonemeal(final ServerLevel level, final RandomSource random, final BlockPos pos, final BlockState state) {
+        popResource(level, pos, new ItemStack(this));
     }
 }
