6db16e609561cfa031dba20b2772a3d36836b8d7
PadjokeJ <jonatan@padjokej.dev>
2026-06-23 08:47:47 +0200

    feat: translation for chemtech suit

--- src/main/java/dev/padjokej/datagen/language/ArcaneEnglishProvider.java
+++ src/main/java/dev/padjokej/datagen/language/ArcaneEnglishProvider.java
@@ -6,10 +6,20 @@ import dev.padjokej.registry.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.world.item.equipment.ArmorType;
 
+import java.util.Map;
+import java.util.StringJoiner;
 import java.util.concurrent.CompletableFuture;
 
 public class ArcaneEnglishProvider extends FabricLanguageProvider {
+
+    Map<ArmorType, String> armorTranslationMap = Map.of(
+            ArmorType.HELMET, "Helmet",
+            ArmorType.CHESTPLATE, "Chestplate",
+            ArmorType.LEGGINGS, "Leggings",
+            ArmorType.BOOTS, "Boots"
+    );
 
     public ArcaneEnglishProvider(FabricPackOutput packOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
         super(packOutput, "en_us", registryLookup);
@@ -21,5 +31,13 @@ public class ArcaneEnglishProvider extends FabricLanguageProvider {
         translationBuilder.add(ModItems.SHIMMER_VIAL, "Shimmer Vial");
         translationBuilder.add(ModBlocks.ZAUN_FLOWER_BLOCK, "Zaun Flower");
         translationBuilder.add(ModEffects.SHIMMER.value(), "Shimmer");
+
+        ModItems.CHEMTECH_SUIT.forEach((a, i) -> {
+            StringJoiner sj = new StringJoiner(" ");
+            sj.add("Chemtech");
+            sj.add(armorTranslationMap.get(a));
+
+            translationBuilder.add(i, sj.toString());
+        });
     }
 }
--- src/main/java/dev/padjokej/datagen/language/ArcaneFrenchProvider.java
+++ src/main/java/dev/padjokej/datagen/language/ArcaneFrenchProvider.java
@@ -6,10 +6,20 @@ import dev.padjokej.registry.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.world.item.equipment.ArmorType;
 
+import java.util.Map;
+import java.util.StringJoiner;
 import java.util.concurrent.CompletableFuture;
 
 public class ArcaneFrenchProvider extends FabricLanguageProvider {
+
+    Map<ArmorType, String> armorTranslationMap = Map.of(
+            ArmorType.HELMET, "Casque",
+            ArmorType.CHESTPLATE, "Plastron",
+            ArmorType.LEGGINGS, "Jambières",
+            ArmorType.BOOTS, "Bottes"
+    );
 
     public ArcaneFrenchProvider(FabricPackOutput packOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
         super(packOutput, "fr_fr", registryLookup);
@@ -17,9 +27,17 @@ public class ArcaneFrenchProvider extends FabricLanguageProvider {
 
     @Override
     public void generateTranslations(HolderLookup.Provider registryLookup, TranslationBuilder translationBuilder) {
-        translationBuilder.add(ModItems.CHEMTECH_BOMB, "Bombe chemtech");
-        translationBuilder.add(ModItems.SHIMMER_VIAL, "Fiole de shimmer");
+        translationBuilder.add(ModItems.CHEMTECH_BOMB, "Bombe Chemtech");
+        translationBuilder.add(ModItems.SHIMMER_VIAL, "Fiole de Shimmer");
         translationBuilder.add(ModBlocks.ZAUN_FLOWER_BLOCK, "Fleure de Zaun");
         translationBuilder.add(ModEffects.SHIMMER.value(), "Shimmer");
+
+        ModItems.CHEMTECH_SUIT.forEach((a, i) -> {
+            StringJoiner sj = new StringJoiner(" ");
+            sj.add(armorTranslationMap.get(a));
+            sj.add("Chemtech");
+
+            translationBuilder.add(i, sj.toString());
+        });
     }
 }
